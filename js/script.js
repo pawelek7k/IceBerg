1c6866e649fd66bde652677b875179a5fb6bd4cf
pawelekk <144838381+pawelek7k@users.noreply.github.com>
2024-01-23 22:00:40 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+function headerEffects() {
+    const activeDiv = document.querySelector(".active-header div")
+
+    const header = document.querySelector(".header");
+
+    window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 100) {
+        header.classList.add("sticky")
+        activeDiv.style.display = "none"
+    } else {
+        header.classList.remove("sticky")
+        activeDiv.style.display = "block"
+    }
+})
+}
+
+headerEffects()
+
+window.onload = function() {
+    const hamburgerBtn = document.getElementById("hamburger-btn");
+    const mobile_menu = document.querySelector('.container-mobile-menu');
+
+	hamburgerBtn.addEventListener('click', () => {
+		hamburgerBtn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
+
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const slider = document.getElementById("slider");
     let slideIndex = 0;
@@ -38,21 +69,41 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-const inputHeroHtml = document.querySelector(".index-hero-input");
-
-const textsArr = [
-    "Najokrutniejsze morderstwa",
-    "Słynni Seryjni Mordercy i Ich Zabójcze Zestawy Umiejętności",
-    "Jak Zabójcy Są Ścigani?",
-    "Ciemna Strona Umysłu: Co Skłania Ludzi do Zabójstw?",
-    "Jak Przestępczość Staje się Sensacją?",
-    "Światowe Zbrodnie: Tajemnicze Przestępstwa, Które Wstrząsnęły Światem",
-    "Morderstwo i Motyw", "Profilowanie Kryminalne"
-  ];
-
 function displayRandomText() {
+    const inputHeroHtml = document.querySelector(".index-hero-input");
+    const textsArr = [
+        "Najokrutniejsze morderstwa",
+        "Słynni Seryjni Mordercy i Ich Zabójcze Zestawy Umiejętności",
+        "Jak Zabójcy Są Ścigani?",
+        "Ciemna Strona Umysłu: Co Skłania Ludzi do Zabójstw?",
+        "Jak Przestępczość Staje się Sensacją?",
+        "Światowe Zbrodnie: Tajemnicze Przestępstwa, Które Wstrząsnęły Światem",
+        "Morderstwo i Motyw", "Profilowanie Kryminalne"
+      ];
     const randomIndex = Math.floor(Math.random() * textsArr.length);
     inputHeroHtml.value = textsArr[randomIndex]
   }
   setInterval(displayRandomText, 5000);
+
+function hiddenText() {
+    let hiddenTextList = document.querySelectorAll(".hidden-text");
+    hiddenTextList.forEach(function(element) {
+        let currentStatus = element.style.display;
+        if (currentStatus === "none" || currentStatus === "") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let hamburgerButton = document.querySelector(".hamburger-btn");
+    let hamburgerMenu = document.querySelector(".hamburger-menu");
+
+    hamburgerButton.addEventListener("click", () => {
+        hamburgerButton.classList.toggle("active-hamburger");
+        hamburgerMenu.classList.toggle("active-menu");
+    });
+});
 
